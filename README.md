ad1cb8aff6313a495f9b8f6c1fd21831b38be8db
nikitalita <69168929+nikitalita@users.noreply.github.com>
2024-12-18 14:00:28 -0800

    add automerge submodule, add to build system

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # patchwork_editor
 
 To build this, clone godotengine/godot @ master, then clone this repository into the `modules/patchwork_editor` directory.
+
+```
+git clone https://github.com/godotengine/godot
+cd godot/modules
+git clone https://github.com/nikitalita/patchwork_editor patchwork_editor --recurse-submodules
+
+```
--- editor/patchwork_editor.cpp
+++ editor/patchwork_editor.cpp
@@ -1,7 +1,7 @@
-#include "editor/patchwork_editor.h"
-#include "modules/patchwork_editor/editor/patchwork_editor.h"
-
-//	PatchworkEditor(EditorNode *p_editor);
+#include "patchwork_editor.h"
+#include "automerge-c/automerge.h"
+#include "automerge-c/utils/stack.h"
+#include "automerge-c/utils/stack_callback_data.h"
 
 PatchworkEditor::PatchworkEditor() {
 }
@@ -9,6 +9,14 @@ PatchworkEditor::PatchworkEditor() {
 PatchworkEditor::~PatchworkEditor() {
 }
 
+static bool abort_cb(AMstack **, void *) {
+	return true;
+}
+
 PatchworkEditor::PatchworkEditor(EditorNode *p_editor) {
 	editor = p_editor;
+	// Just testing linkage to automerge-c
+	AMstack *stack = NULL;
+	AMdoc *doc1;
+	AMitemToDoc(AMstackItem(&stack, AMcreate(NULL), abort_cb, AMexpect(AM_VAL_TYPE_DOC)), &doc1);
 }
