23e8d7b185e60b57cbd09aa95b117198c22ed3c5
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-02-06 15:45:42 -0600

    move rust to plugin, remove most of everything
    The only thing we need in the editor now is just a handful of functions that aren't exposed in the GDExtension API

--- thirdparty/patchwork_rust/include/patchwork_rust/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-// #include <patchwork_rust/patchwork_rust.h>
-//
-// namespace patchwork_rust {
-// AutomergeFS *CreateAutomergeFS(const char *maybe_fs_doc_id, void *parent_class, AutoMergeSignalCallback signal_callback);
-// }
-#endif //UTIL_H
--- thirdparty/patchwork_rust/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <patchwork_rust/patchwork_rust.h>
-#include <patchwork_rust/util.h>
-//
-// namespace patchwork_rust {
-// AutomergeFS *CreateAutomergeFS(const char *maybe_fs_doc_id, void *parent_class, AutoMergeSignalCallback signal_callback) {
-// 	return automerge_fs_create(maybe_fs_doc_id, parent_class, signal_callback);
-// }
-// } //namespace patchwork_rust
